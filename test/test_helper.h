5d430e24aa7954f6cdd8ba420a315ab1fe811d54
Chase Conklin <chase@conklins.net>
2015-01-20 10:24:38 -0500

    added new assertion

--- test/test_helper.h
+++ test/test_helper.h
@@ -2,3 +2,4 @@
 void * _l, * _r;
 #define assertEqual(l, r) if ((_l = l) != (_r = r)) { printf("Assertion Failed: assertEqual(%s, %s)\n    %llu != %llu (%p != %p)\n# %s:%d\n", #l, #r, (unsigned long long int) _l, (unsigned long long int) _r, _l, _r, __FILE__, __LINE__); exit(1); }
 #define assertNotEqual(l, r) if ((_l = l) == (_r = r)) { printf("Assertion Failed: assertNotEqual(%s, %s)\n    %llu != %llu (%p == %p)\n# %s:%d\n", #l, #r, (unsigned long long int) _l, (unsigned long long int) _r, _l, _r, __FILE__, __LINE__); exit(1); }
+#define assertNotReaches() { printf("Assertion Failed: Arrived at unreachable point\n# %s:%d\n", __FILE__, __LINE__); exit(1); }
