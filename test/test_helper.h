1deae0d7c5edd21db76e0c0081f1b6cae7c3358c
Chase Conklin <chase@conklins.net>
2015-01-23 22:37:38 -0500

    using tester to run tests

--- test/test_helper.h
+++ test/test_helper.h
@@ -1,5 +1,5 @@
-#include <assert.h>
 void * _l, * _r;
+#define assert(s) if (! (s)) { printf("Assertion Failed: assert(%s)\n# %s:%d\n", #s , __FILE__, __LINE__); exit(1); }
 #define assertEqual(l, r) if ((_l = l) != (_r = r)) { printf("Assertion Failed: assertEqual(%s, %s)\n    %llu != %llu (%p != %p)\n# %s:%d\n", #l, #r, (unsigned long long int) _l, (unsigned long long int) _r, _l, _r, __FILE__, __LINE__); exit(1); }
 #define assertNotEqual(l, r) if ((_l = l) == (_r = r)) { printf("Assertion Failed: assertNotEqual(%s, %s)\n    %llu != %llu (%p == %p)\n# %s:%d\n", #l, #r, (unsigned long long int) _l, (unsigned long long int) _r, _l, _r, __FILE__, __LINE__); exit(1); }
 #define assertNotReaches() { printf("Assertion Failed: Arrived at unreachable point\n# %s:%d\n", __FILE__, __LINE__); exit(1); }
