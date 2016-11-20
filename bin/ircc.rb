2dc69e420ef36185472d8f81845a335a4fdc5346
Chase Conklin <chase@conklins.net>
2016-11-20 17:12:36 -0500

    add debug flag

--- bin/ircc.rb
+++ bin/ircc.rb
@@ -6,12 +6,15 @@ require_relative "../lib/translator"
 require_relative "../lib/generator"
 require_relative "../lib/compiler"
 
-options = {output: "a.out"}
+options = {output: "a.out", debug: false}
 
 parser = OptionParser.new do |opts|
   opts.banner = "Usage: ircc [options] file"
   opts.on "-o", "--output FILE", "Output Binary" do |o|
     options[:output] = o 
+  end
+  opts.on "-g", "Emit Debug Symbols" do |g|
+    options[:debug] = g
   end
 end
 
@@ -30,5 +33,5 @@ processed_tree = Translator.translate! tree
 
 generator = Generator.new(processed_tree[:callables], processed_tree[:tree])
 
-Compiler.compile(generator.generate, output: options[:output])
+Compiler.compile(generator.generate, output: options[:output], debug: options[:debug])
 
--- lib/compiler.rb
+++ lib/compiler.rb
@@ -1,8 +1,8 @@
 module Compiler
   extend self
 
-  def compile(code, output: "a.out")
-    IO.popen "/usr/bin/clang -o #{output} -xc -", "w" do |io|
+  def compile(code, output: "a.out", debug: false)
+    IO.popen "/usr/bin/clang -o #{output} #{debug ? '-g' : ''} -xc -", "w" do |io|
       io.write code
     end
   end
