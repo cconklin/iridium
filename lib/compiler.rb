3ffea2a14244c12a76d4f6722e6d5f605cad0c49
Chase Conklin <chase@conklins.net>
2017-07-07 22:18:21 -0500

    add debug capability to compiler

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module Compiler
   extend self
 
@@ -5,13 +7,20 @@ module Compiler
   LDFLAGS = `pkg-config --libs bdw-gc`.strip
 
   def compile(code, output: "a.out", debug: false, link: true)
+    fname = SecureRandom.hex + ".c"
     command = if link
-      "/usr/bin/clang #{LDFLAGS} -o #{output} #{debug ? '-g' : ''} #{IRIDIUM} -xc -"
+      "/usr/bin/clang #{LDFLAGS} -o #{output} #{debug ? '-g' : ''} #{IRIDIUM} -xc #{debug ? fname : '-'}"
     else
-      "/usr/bin/clang -o #{output} -c #{debug ? '-g' : '' } -xc -"
+      "/usr/bin/clang -o #{output} -c #{debug ? '-g' : '' } -xc #{debug ? fname : '-'}"
+    end
+    if debug
+      puts command
+      File.open(fname, "w") do |f|
+        f.write code
+      end
     end
     IO.popen command, "w" do |io|
-      io.write code
+      io.write code unless debug
     end
   end
 
